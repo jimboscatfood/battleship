46c2acb820ab6eed20ad125a75804b4bbfb8291f
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2025-10-11 22:19:29 +0800

    Created the Gameboard factory and associated tests
    Added methods for placing ships on specific coordinates on the board,
    receiving attacks, and reporting whether all ships are sunk.
    
    Since the methods of placing ships and receiving attacks cause side
    effects only but they will be exposed publicly for use by other modules,
    the tests mainly involve getting the board property and checking its
    state after calling these methods.

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,6 @@
-export { Ship }
+import { dir } from 'console'
+
+export { Ship, Gameboard }
 
 //ship factory to be instantiated at gameboard
 function Ship(shipSize) {
@@ -30,5 +32,110 @@ function Ship(shipSize) {
     return {
         hit,
         getSunk,
+    }
+}
+
+//gameboard factory to be instantiated for each player
+function Gameboard() {
+    const xAxisLength = 10
+    const yAxisLength = 10
+    let board = Array.from({ length: yAxisLength }, () =>
+        Array.from({ length: xAxisLength }, () => null)
+    )
+
+    function checkValidPlacement(shipSize, startCoor, direction) {
+        const currentBoard = getBoard()
+        const startX = startCoor[0]
+        const startY = startCoor[1]
+        let valid = false
+        //IF the starting coordinate is within the board
+        if (startX < xAxisLength && startY < yAxisLength) {
+            //IF the placement direction is along x axis and the proposed placement is contained within the board
+            if (direction == 'x' && startX + shipSize - 1 < xAxisLength) {
+                //IF the proposed placement does not interfere with any existing ships
+                let numOfSpace = 0
+                for (let i = 0; i < shipSize; i++) {
+                    currentBoard[startY][startX + i] === null
+                        ? (numOfSpace += 1)
+                        : null
+                }
+                numOfSpace === shipSize ? (valid = true) : null
+            } else if (
+                direction == 'y' &&
+                startY + shipSize - 1 < yAxisLength
+            ) {
+                let numOfSpace = 0
+                for (let j = 0; j < shipSize; j++) {
+                    currentBoard[startY + j][startX] === null
+                        ? (numOfSpace += 1)
+                        : null
+                }
+                numOfSpace === shipSize ? (valid = true) : null
+            }
+        }
+        return valid
+    }
+
+    function placeShip(shipSize, startCoor, direction) {
+        const newShip = Ship(shipSize)
+        //store a reference of the newShip object to each of the space it occupies in the 2D array
+        //only place ship if its startCoor and its remains are within the gameboard
+        const startX = startCoor[0]
+        const startY = startCoor[1]
+        //IF the placement is valid
+        if (checkValidPlacement(shipSize, startCoor, direction)) {
+            //IF the ship is placed horizontally
+            if (direction == 'x') {
+                for (let i = 0; i < shipSize; i++) {
+                    //REPLACE null on the 2D array with a reference of the newShip object
+                    board[startY][startX + i] = newShip
+                }
+            }
+            //ELSE IF the ship is placed vertically
+            else if (direction == 'y') {
+                for (let j = 0; j < shipSize; j++) {
+                    //REPLACE null on the 2D array with a reference of the newShip object
+                    board[startY + j][startX] = newShip
+                }
+            }
+        }
+    }
+
+    function receiveAttack(targetCoor) {
+        const xCoor = targetCoor[0]
+        const yCoor = targetCoor[1]
+        const targetObj = board[yCoor][xCoor]
+        //IF targetObj is an actual ship object
+        if (targetObj !== null && targetObj !== true && targetObj !== false) {
+            targetObj.hit()
+            //REPLACE ship object with true, translating to a hit
+            board[yCoor][xCoor] = true
+        } else {
+            //REPLACE null object with false, translating to a miss
+            board[yCoor][xCoor] = false
+        }
+    }
+
+    function checkIfAllSunk() {
+        for (let i = 0; i < yAxisLength; i++) {
+            let shipsNotSunk = board[i].filter(
+                (cell) => cell !== null && cell !== true && cell !== false
+            )
+            if (shipsNotSunk.length > 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    function getBoard() {
+        return board
+    }
+
+    return {
+        placeShip,
+        receiveAttack,
+        checkIfAllSunk,
+        getBoard,
     }
 }
--- src/setup.test.js
+++ src/setup.test.js
@@ -1,4 +1,4 @@
-import { Ship } from './setup'
+import { Ship, Gameboard } from './setup'
 
 //tests for Ship factory
 describe('tests for Ship objects', () => {
@@ -27,5 +27,102 @@ describe('tests for Ship objects', () => {
         shipFour.hit()
         shipFour.hit()
         expect(shipFour.getSunk()).toBeTruthy()
+    })
+})
+
+//test for Gameboard factory
+describe('test for Gameboard object', () => {
+    test('test if the board is initiated as a 10x10 grid', () => {
+        const testBoard = Gameboard()
+        const curTestBoard = testBoard.getBoard()
+        expect(curTestBoard).toHaveLength(10)
+        expect(curTestBoard[0]).toHaveLength(10)
+        expect(curTestBoard[9]).toHaveLength(10)
+    })
+
+    test('test placing a ship of size 2 at row 0 col 0 in the x dir', () => {
+        const testBoard = Gameboard()
+        testBoard.placeShip(2, [0, 0], 'x')
+        const curTestBoard = testBoard.getBoard()
+        //check the occupied cells
+        expect(curTestBoard[0][0]).not.toBeNull()
+        expect(curTestBoard[0][1]).not.toBeNull()
+        //check the neighbouring cells
+        expect(curTestBoard[0][2]).toBeNull()
+        expect(curTestBoard[1][0]).toBeNull()
+    })
+
+    test('test placing a ship of size 5 at row 4 col 4 in the y dir', () => {
+        const testBoard = Gameboard()
+        testBoard.placeShip(5, [4, 4], 'y')
+        const curTestBoard = testBoard.getBoard()
+        //check the occupied cells
+        expect(curTestBoard[4][4]).not.toBeNull()
+        expect(curTestBoard[5][4]).not.toBeNull()
+        expect(curTestBoard[8][4]).not.toBeNull()
+        //check the neighbouring cells
+        expect(curTestBoard[9][4]).toBeNull()
+        expect(curTestBoard[4][5]).toBeNull()
+        expect(curTestBoard[8][5]).toBeNull()
+    })
+
+    test('test overlapping placement', () => {
+        const testBoard = Gameboard()
+        //first place a ship of size 5 at [4,4] along y
+        testBoard.placeShip(5, [4, 4], 'y')
+        //then place a second overlapping ship of size 2 at [4,4] along x
+        testBoard.placeShip(2, [4, 4], 'x')
+        //then place a third ship of size 3 at [2,4]
+        testBoard.placeShip(3, [2, 4], 'x')
+        //then place a fourth ship of size 3 at [1,5]
+        testBoard.placeShip(3, [1, 5], 'x')
+        const curTestBoard = testBoard.getBoard()
+        //check if the first ship occupies the designated
+        expect(curTestBoard[4][4]).not.toBeNull()
+        expect(curTestBoard[8][4]).not.toBeNull()
+        //check if the second ship has been created by checking if the cell is null next to [4,4]
+        expect(curTestBoard[4][5]).toBeNull()
+        //check if the third ship has been created by checking if the cell is null at [2,4]
+        expect(curTestBoard[4][2]).toBeNull()
+        //check if the third ship has been created by checking if the cell is null at [1,4]
+        expect(curTestBoard[5][1]).not.toBeNull()
+    })
+
+    test('test for receiving a attack', () => {
+        const testBoard = Gameboard()
+        //place a ship of size 5 at [4,4] along y
+        testBoard.placeShip(5, [4, 4], 'y')
+        testBoard.receiveAttack([4, 4])
+        const curTestBoard = testBoard.getBoard()
+        expect(curTestBoard[4][4]).toBe(true)
+    })
+
+    test('test for missing a attack', () => {
+        const testBoard = Gameboard()
+        //place a ship of size 5 at [4,4] along y
+        testBoard.placeShip(5, [4, 4], 'y')
+        testBoard.receiveAttack([0, 0])
+        const curTestBoard = testBoard.getBoard()
+        expect(curTestBoard[0][0]).toBe(false)
+    })
+
+    test('test for checking if all ships are sunk with 1 ship of size 2 taking 2 hits', () => {
+        const testBoard = Gameboard()
+        //place a ship of size 2 at [4,4] along y
+        testBoard.placeShip(2, [4, 4], 'y')
+        testBoard.receiveAttack([4, 4])
+        testBoard.receiveAttack([4, 5])
+        expect(testBoard.checkIfAllSunk()).toBe(true)
+    })
+
+    test('test for checking if all ships are sunk with 2 ship of size 2, and just 1 of them taking 2 hits', () => {
+        const testBoard = Gameboard()
+        //place a ship of size 2 at [4,4] along y
+        testBoard.placeShip(2, [4, 4], 'y')
+        //place another ship of size 2 at [0,0] along x
+        testBoard.placeShip(2, [0, 0], 'x')
+        testBoard.receiveAttack([4, 4])
+        testBoard.receiveAttack([4, 5])
+        expect(testBoard.checkIfAllSunk()).toBe(false)
     })
 })
